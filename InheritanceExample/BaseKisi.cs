a6b5069bdbd67dcc93cfce477b693735ece5812c
rabiademirbag <rddemirbagg@gmail.com>
2025-05-21 19:24:44 +0300

    Proje dosyası ekle.

---
+++ InheritanceExample/BaseKisi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    public class BaseKisi
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public void DisplayNameSurname()
+        {
+            Console.WriteLine("Ad Soyad: " + Name + " " + Surname);
+        }
+
+    }
+    public class Ogrenci : BaseKisi
+    {
+        public string OgrenciNumarasi { get; set; }
+ 
+        public void DisplayOgrenci()
+        {
+          
+            DisplayNameSurname();
+            Console.WriteLine("Ogrenci no : " + OgrenciNumarasi);
+        }
+    }
+    public class Ogretmen : BaseKisi
+    {
+        public decimal Salary { get; set; }
+
+        public void DisplayOgretmen()
+        {
+            DisplayNameSurname();
+            Console.WriteLine("Maas : " + Salary);
+
+        }
+    }
+}
---
+++ InheritanceExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using InheritanceExample;
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        Ogretmen ogretmen = new Ogretmen();
+        Ogrenci ogrenci=new Ogrenci();
+        ogretmen.Name = "Musa";
+        ogretmen.Surname = "Aydın";
+        ogretmen.Salary = 86000;
+
+        ogretmen.DisplayOgretmen();
+        ogrenci.Name = "Rana";
+        ogrenci.Surname = "Kaya";
+        ogrenci.OgrenciNumarasi = "1921221032";
+        ogrenci.DisplayOgrenci();
+    }
+}
